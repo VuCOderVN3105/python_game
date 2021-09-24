8597938fbc3616c6c1758860535c640a9dc73db0
VuCOderVN3105 <luuvu3477@gmail.com>
2021-09-24 15:09:28 +0700

    update

--- pythongame.py
+++ pythongame.py
@@ -5,10 +5,11 @@ pygame.init()
 screen = pygame.display.set_mode((1080,480))
 pygame.display.set_caption('First Game in Pygame')
 clock = pygame.time.Clock()
+icon = pygame.image.load('C:/Users/Public/Pics/macOS BigSur ICONS by Maiguris/Visual Studio Code Regular.png')
 running = True
 while running:
     clock.tick(60)
-    pygame.draw.line(screen,(0,0), (1080,480), 5)
+    screen.blit(icon,(0,0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
